dcb6c6c14b1b69fdc4529ae577a8482356a56531
Noah Guale <noahguale@Noahs-MacBook-Pro.local>
2023-04-27 13:07:19 -0500

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import "../styles/index.css";
 import "@code-hike/mdx/dist/index.css";
 import { seo } from "@/lib/seo";
 import { DefaultSeo } from "next-seo";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...seo} />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
