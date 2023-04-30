810018348bd11d6c24b5b12da6ac756701d3751d
Noah Guale <noahguale@Noahs-MacBook-Pro.local>
2023-04-30 15:29:59 -0500

    index

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,11 +10,6 @@ export default function Document() {
 
         <meta name="theme-color" content="#181818" />
         <meta name="google-site-verification" content="FaK1rSoPG3Ae5c8uL_BQJMVVdWLOAtgSjNrVvb7dOU0" />
-        <link
-          rel="preload"
-          as="image"
-          href="https://res.cloudinary.com/delba/image/upload/h_500/bg_gradient_pfosr9"
-        />
       </Head>
 
       <body className="antialiased selection:bg-stone-500/90 selection:text-white">
