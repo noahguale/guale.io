beee0c9521f8f711c40ec48cd3b888d39c1a5cfc
Noah Guale <noahguale@Noahs-MacBook-Pro.local>
2023-04-30 10:01:08 -0500

    favicon fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,10 @@ export default function Document() {
   return (
     <Html lang="en" className="scroll-p-24">
       <Head>
-        <link rel="shortcut icon" href="../public/favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="../public/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon/favicon-16x16.png"/>
+        <link rel="shortcut icon" href="favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png"/>
 
         <meta name="theme-color" content="#181818" />
         <meta name="google-site-verification" content="FaK1rSoPG3Ae5c8uL_BQJMVVdWLOAtgSjNrVvb7dOU0" />
